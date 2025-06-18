18312bc7ad399461b601426ef5c481833ebfb951
Alex Rosenthal <arosenthal1298@gmail.com>
2025-06-17 22:50:47 -0400

    .baseline

--- mobile/watch-fax/app/(auth)/sign-up-page.tsx
+++ mobile/watch-fax/app/(auth)/sign-up-page.tsx
@@ -23,81 +23,103 @@ const SignUpPage = () => {
     const [confirmPassword, setConfirmPassword] = useState<string>('');
 
     return (
-        <TouchableWithoutFeedback
-            onPress={Keyboard.dismiss}
-            style={{
-                borderWidth: 1,
-                borderColor: 'red',
-                backgroundColor: 'green',
-                zIndex: 1000,
-            }}
-        >
-            <SafeAreaView style={defaultStyles.container}>
-                <View
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <SafeAreaView style={[defaultStyles.container, { paddingTop: 20 }]}>
+                <KeyboardAvoidingView
+                    keyboardVerticalOffset={-200}
+                    behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
                     style={{
-                        borderWidth: 1,
-                        borderColor: 'blue',
                         flex: 1,
-                        alignItems: 'center',
+                        //width: '100%',
+                        backgroundColor: backgroundColor,
                     }}
                 >
-                    <View style={{ marginBottom: 20 }}>
-                        <Text style={styles.textLabel}>Email</Text>
-                        <TextInput
-                            autoCapitalize='none'
-                            placeholder='Email'
-                            placeholderTextColor='gray'
-                            onChangeText={setEmail}
-                            value={email}
-                            style={styles.textInput}
-                        />
-                    </View>
-                    <View style={{ marginBottom: 20 }}>
-                        <Text style={styles.textLabel}>Password</Text>
-                        <TextInput
-                            autoCapitalize='none'
-                            placeholder='Password'
-                            placeholderTextColor='gray'
-                            onChangeText={setPassword}
-                            value={password}
-                            style={styles.textInput}
-                        />
-                    </View>
-                    <View style={{ marginBottom: 20 }}>
-                        <Text style={styles.textLabel}>Confirm password</Text>
-                        <TextInput
-                            autoCapitalize='none'
-                            placeholder='Password'
-                            placeholderTextColor='gray'
-                            onChangeText={setConfirmPassword}
-                            value={confirmPassword}
-                            style={styles.textInput}
-                        />
-                    </View>
-                    <TouchableOpacity
+                    <View
                         style={{
-                            backgroundColor: navyColor,
-                            padding: 10,
-                            borderRadius: 10,
                             alignItems: 'center',
-                            width: 320,
-                            justifyContent: 'center',
-                            height: 50,
-                            marginBottom: 8,
                         }}
-                        //onPress={onSignInPress}
                     >
-                        <Text
+                        <View
                             style={{
-                                color: 'white',
-                                fontSize: 24,
-                                fontFamily: 'roboto-black',
+                                marginBottom: 20,
+                                width: '100%',
+                                display: 'flex',
+                                alignItems: 'center',
                             }}
                         >
-                            Continue
-                        </Text>
-                    </TouchableOpacity>
-                </View>
+                            <Text style={styles.textLabel}>Email</Text>
+                            <TextInput
+                                autoCapitalize='none'
+                                placeholder='Email'
+                                placeholderTextColor='gray'
+                                onChangeText={setEmail}
+                                value={email}
+                                style={styles.textInput}
+                            />
+                        </View>
+                        <View
+                            style={{
+                                marginBottom: 20,
+                                width: '100%',
+                                display: 'flex',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <Text style={styles.textLabel}>Password</Text>
+                            <TextInput
+                                autoCapitalize='none'
+                                placeholder='Password'
+                                placeholderTextColor='gray'
+                                onChangeText={setPassword}
+                                value={password}
+                                style={styles.textInput}
+                            />
+                        </View>
+                        <View
+                            style={{
+                                marginBottom: 20,
+                                width: '100%',
+                                display: 'flex',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <Text style={styles.textLabel}>
+                                Confirm password
+                            </Text>
+                            <TextInput
+                                autoCapitalize='none'
+                                placeholder='Password'
+                                placeholderTextColor='gray'
+                                onChangeText={setConfirmPassword}
+                                value={confirmPassword}
+                                style={styles.textInput}
+                            />
+                        </View>
+                        <TouchableOpacity
+                            style={{
+                                backgroundColor: navyColor,
+                                padding: 10,
+                                borderRadius: 10,
+                                alignItems: 'center',
+                                width: '90%',
+                                justifyContent: 'center',
+                                height: 50,
+                                marginBottom: 8,
+                            }}
+                            //onPress={onSignInPress}
+                        >
+                            <Text
+                                style={{
+                                    color: 'white',
+                                    fontSize: 24,
+                                    fontFamily: 'roboto-black',
+                                }}
+                            >
+                                Continue
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                </KeyboardAvoidingView>
             </SafeAreaView>
         </TouchableWithoutFeedback>
     );
@@ -105,15 +127,16 @@ const SignUpPage = () => {
 
 const styles = StyleSheet.create({
     textLabel: {
+        alignSelf: 'flex-start',
         color: navyColor,
         fontSize: 24,
-        marginLeft: 10,
+        marginLeft: 30,
         fontFamily: 'roboto-black',
     },
     textInput: {
         color: 'black',
         height: 50,
-        width: 320,
+        width: '90%',
         borderColor: '#D4D4D4',
         borderWidth: 1,
         borderRadius: 10,
