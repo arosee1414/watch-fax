a31f84e1113874797454fdb329dda9f16811305d
Alex Rosenthal <arosenthal1298@gmail.com>
2025-06-19 19:50:14 -0400

    add continue button

--- mobile/watch-fax/app/(screens)/add-a-watch-1.tsx
+++ mobile/watch-fax/app/(screens)/add-a-watch-1.tsx
@@ -1,6 +1,15 @@
 import { backgroundColor, navyColor } from '@/assets/default-styles';
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, SafeAreaView, TextInput } from 'react-native';
+import {
+    StyleSheet,
+    View,
+    Text,
+    SafeAreaView,
+    TextInput,
+    KeyboardAvoidingView,
+    Platform,
+    TouchableOpacity,
+} from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { Float } from 'react-native/Libraries/Types/CodegenTypes';
 import CurrencyInput from 'react-native-currency-input';
@@ -24,79 +33,108 @@ const AddAWatch1 = () => {
 
     return (
         <SafeAreaView style={{ backgroundColor: backgroundColor, flex: 1 }}>
-            <ScrollView
-                style={{
-                    flex: 1,
-                    paddingVertical: 20,
-                    paddingHorizontal: 40,
-                }}
-                contentContainerStyle={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                }}
+            <KeyboardAvoidingView
+                keyboardVerticalOffset={100}
+                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                style={{ flex: 1 }}
             >
-                <View style={{ marginBottom: 20, width: '100%' }}>
-                    <Text style={styles.textLabel}>Brand</Text>
-                    <TextInput
-                        autoCapitalize='none'
-                        placeholder='Select a brand'
-                        placeholderTextColor='gray'
-                        onChangeText={setBrand}
-                        value={brand}
-                        style={styles.textInput}
-                    />
-                </View>
-                <View style={{ marginBottom: 20, width: '100%' }}>
-                    <Text style={styles.textLabel}>Purchase price</Text>
-                    <CurrencyInput
-                        style={styles.textInput}
-                        placeholder='$0.00'
-                        placeholderTextColor='gray'
-                        value={price ?? null}
-                        onChangeValue={setPrice}
-                        prefix='$'
-                        delimiter=','
-                        separator='.'
-                        precision={2}
-                        minValue={0}
-                    />
-                </View>
-                <View style={{ marginBottom: 20, width: '100%' }}>
-                    <Text style={styles.textLabel}>Reference number</Text>
-                    <TextInput
-                        autoCapitalize='none'
-                        placeholder='Enter the reference number'
-                        placeholderTextColor='gray'
-                        onChangeText={setReferenceNumber}
-                        value={referenceNumber}
-                        style={styles.textInput}
-                    />
-                </View>
-                <View style={{ marginBottom: 20, width: '100%' }}>
-                    <Text style={styles.textLabel}>Serial number</Text>
-                    <TextInput
-                        autoCapitalize='none'
-                        placeholder='Enter the serial number'
-                        placeholderTextColor='gray'
-                        onChangeText={setSerialNumber}
-                        value={serialNumber}
-                        style={styles.textInput}
-                    />
-                </View>
-                <View style={{ marginBottom: 20, width: '100%' }}>
-                    <Text style={styles.textLabel}>Production year</Text>
-                    <TextInput
-                        keyboardType='numeric'
-                        style={styles.textInput}
-                        placeholder='Enter the production year'
-                        placeholderTextColor='gray'
-                        value={productionYear?.toString()}
-                        onChangeText={convertToNumber}
-                        maxLength={4}
-                    />
-                </View>
-            </ScrollView>
+                <ScrollView
+                    style={{
+                        flex: 1,
+                        paddingVertical: 20,
+                        paddingHorizontal: 40,
+                    }}
+                    contentContainerStyle={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    <View style={{ marginBottom: 20, width: '100%' }}>
+                        <Text style={styles.textLabel}>Brand</Text>
+                        <TextInput
+                            autoCapitalize='none'
+                            placeholder='Select a brand'
+                            placeholderTextColor='gray'
+                            onChangeText={setBrand}
+                            value={brand}
+                            style={styles.textInput}
+                        />
+                    </View>
+                    <View style={{ marginBottom: 20, width: '100%' }}>
+                        <Text style={styles.textLabel}>Purchase price</Text>
+                        <CurrencyInput
+                            style={styles.textInput}
+                            placeholder='$0.00'
+                            placeholderTextColor='gray'
+                            value={price ?? null}
+                            onChangeValue={setPrice}
+                            prefix='$'
+                            delimiter=','
+                            separator='.'
+                            precision={2}
+                            minValue={0}
+                        />
+                    </View>
+                    <View style={{ marginBottom: 20, width: '100%' }}>
+                        <Text style={styles.textLabel}>Reference number</Text>
+                        <TextInput
+                            autoCapitalize='none'
+                            placeholder='Enter the reference number'
+                            placeholderTextColor='gray'
+                            onChangeText={setReferenceNumber}
+                            value={referenceNumber}
+                            style={styles.textInput}
+                        />
+                    </View>
+                    <View style={{ marginBottom: 20, width: '100%' }}>
+                        <Text style={styles.textLabel}>Serial number</Text>
+                        <TextInput
+                            autoCapitalize='none'
+                            placeholder='Enter the serial number'
+                            placeholderTextColor='gray'
+                            onChangeText={setSerialNumber}
+                            value={serialNumber}
+                            style={styles.textInput}
+                        />
+                    </View>
+                    <View style={{ marginBottom: 20, width: '100%' }}>
+                        <Text style={styles.textLabel}>Production year</Text>
+                        <TextInput
+                            keyboardType='numeric'
+                            style={styles.textInput}
+                            placeholder='Enter the production year'
+                            placeholderTextColor='gray'
+                            value={productionYear?.toString()}
+                            onChangeText={convertToNumber}
+                            maxLength={4}
+                        />
+                    </View>
+                    <TouchableOpacity
+                        style={{
+                            backgroundColor: navyColor,
+                            padding: 10,
+                            borderRadius: 10,
+                            alignItems: 'center',
+                            width: '100%',
+                            justifyContent: 'center',
+                            height: 50,
+                            marginTop: 100,
+                        }}
+                        //onPress={onSignInPress}
+                    >
+                        <Text
+                            style={{
+                                color: 'white',
+                                fontSize: 24,
+                                fontFamily: 'roboto-black',
+                            }}
+                        >
+                            Continue
+                        </Text>
+                    </TouchableOpacity>
+                </ScrollView>
+            </KeyboardAvoidingView>
         </SafeAreaView>
     );
 };
